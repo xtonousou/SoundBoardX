0c476c76a79b88d328d284decf96cf2a903172bb
xToNouSou <tonousou96>
2016-07-24 08:13:50 +0300

    Updated splash screen

--- app/src/main/java/com/xtonousou/soundboard/SplashScreen.java
+++ app/src/main/java/com/xtonousou/soundboard/SplashScreen.java
@@ -2,11 +2,13 @@ package com.xtonousou.soundboard;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.github.rahatarmanahmed.cpv.CircularProgressView;
 
@@ -18,18 +20,38 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
         CircularProgressView progressView = (CircularProgressView) findViewById(R.id.splash_progress_view);
+        ImageView logo = (ImageView) findViewById(R.id.splash_logo);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            int[][] textState = new int[][] {
+                    new int[] {-android.R.attr.state_enabled}, // disabled
+                    new int[] { android.R.attr.state_enabled}, // enabled
+                    new int[] {-android.R.attr.state_checked}, // unchecked
+                    new int[] { android.R.attr.state_pressed}  // pressed
+            };
+
+            int[] textStateColor = new int[] {
+                    new DayColor(logo.getContext()).getDayColor(),// disabled
+                    new DayColor(logo.getContext()).getDayColor(),// enabled
+                    new DayColor(logo.getContext()).getDayColor(),// unchecked
+                    new DayColor(logo.getContext()).getDayColor() // pressed
+            };
+            logo.setImageTintList(new ColorStateList(textState, textStateColor));
+        } else {
+            logo.setEnabled(false);
+        }
+
         progressView.setColor((new DayColor(progressView.getContext())).getDayColor());
 
         final Thread timerThread = new Thread() {
             public void run() {
                 try {
-                    if (!(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP &&
-                            ((PowerManager) SplashScreen.this.getSystemService(Context.POWER_SERVICE))
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP &&
+                            (!((PowerManager) SplashScreen.this.getSystemService(Context.POWER_SERVICE))
                                     .isPowerSaveMode())) {
                         sleep(1600);
-                    } else {
-                        onPause();
                     }
                 } catch(InterruptedException e) {
                     Log.e(TAG, e.getMessage());
@@ -39,7 +61,6 @@ public class SplashScreen extends AppCompatActivity {
                 }
             }
         };
-
         // start thread
         timerThread.start();
         // start animation
