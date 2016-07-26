3ea7868322e886aa6559b947390efff4790611ae
xToNouSou <tonousou96>
2016-07-26 05:38:44 +0300

    update splash screen

--- app/src/main/java/com/xtonousou/soundboard/SplashScreen.java
+++ app/src/main/java/com/xtonousou/soundboard/SplashScreen.java
@@ -1,77 +1,62 @@
 package com.xtonousou.soundboard;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ImageView;
 
-import com.github.rahatarmanahmed.cpv.CircularProgressView;
+import com.github.jlmd.animatedcircleloadingview.AnimatedCircleLoadingView;
 
 public class SplashScreen extends AppCompatActivity {
 
-    public final static String TAG = "SplashActivity";
+    private AnimatedCircleLoadingView animatedCircleLoadingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        animatedCircleLoadingView = (AnimatedCircleLoadingView) findViewById(R.id.circle_loading_view);
+        startLoading();
+        startPercentMockThread();
+    }
 
-        CircularProgressView progressView = (CircularProgressView) findViewById(R.id.splash_progress_view);
-        ImageView logo = (ImageView) findViewById(R.id.splash_logo);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            int[][] textState = new int[][] {
-                    new int[] {-android.R.attr.state_enabled}, // disabled
-                    new int[] { android.R.attr.state_enabled}, // enabled
-                    new int[] {-android.R.attr.state_checked}, // unchecked
-                    new int[] { android.R.attr.state_pressed}  // pressed
-            };
+    private void startLoading() {
+        animatedCircleLoadingView.startDeterminate();
+    }
 
-            int[] textStateColor = new int[] {
-                    new DayColor(logo.getContext()).getDayColor(),// disabled
-                    new DayColor(logo.getContext()).getDayColor(),// enabled
-                    new DayColor(logo.getContext()).getDayColor(),// unchecked
-                    new DayColor(logo.getContext()).getDayColor() // pressed
-            };
-            logo.setImageTintList(new ColorStateList(textState, textStateColor));
-        } else {
-            logo.setEnabled(false);
-        }
-
-        progressView.setColor((new DayColor(progressView.getContext())).getDayColor());
-
-        final Thread timerThread = new Thread() {
+    private void startPercentMockThread() {
+        Runnable runnable = new Runnable() {
+            @Override
             public void run() {
                 try {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP &&
-                            (!((PowerManager) SplashScreen.this.getSystemService(Context.POWER_SERVICE))
-                                    .isPowerSaveMode())) {
-                        sleep(1600);
+                    Thread.sleep(1500);
+                    for (int i = 0; i <= 100; i++) {
+                        Thread.sleep(5);
+                        changePercent(i);
                     }
-                } catch(InterruptedException e) {
-                    Log.e(TAG, e.getMessage());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 } finally {
                     Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                     startActivity(intent);
                 }
             }
         };
-        // start thread
-        timerThread.start();
-        // start animation
-        progressView.startAnimation();
+        new Thread(runnable).start();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // finish activity
-        finish();
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    private void changePercent(final int percent) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                animatedCircleLoadingView.setPercent(percent);
+            }
+        });
     }
 }
