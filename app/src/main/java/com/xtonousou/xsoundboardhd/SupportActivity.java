c762a3ebb79fedbe1e73bb317f98ef8bc7c32fac
xtonousou <xtonousou96@gmail.com>
2016-08-03 07:45:40 +0300

    fix home button

--- app/src/main/java/com/xtonousou/xsoundboardhd/SupportActivity.java
+++ app/src/main/java/com/xtonousou/xsoundboardhd/SupportActivity.java
@@ -106,10 +106,7 @@ public class SupportActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                // app icon in action bar clicked; go home
-                Intent intent = new Intent(this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
+                finish();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
