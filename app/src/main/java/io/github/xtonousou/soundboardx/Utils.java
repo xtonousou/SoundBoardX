1eff7f1bf979db1d4c4249f0fb8a0a771eba6a3a
xtonousou <xtonousou@gmail.com>
2017-10-24 14:19:28 +0300

    This commit is a lie

---
+++ app/src/main/java/io/github/xtonousou/soundboardx/Utils.java
@@ -0,0 +1,115 @@
+package io.github.xtonousou.soundboardx;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.SearchView;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
+
+import com.romainpiel.shimmer.ShimmerTextView;
+
+public class Utils {
+
+    private boolean  isPainted = false;
+
+    public int getScreenHeight(Activity activity) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        return metrics.heightPixels;
+    }
+
+    public int getScreenWidth(Activity activity) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        return metrics.widthPixels;
+    }
+
+    public int getSelectedColor() {
+        return SharedPrefs.getInstance().getSelectedColor();
+    }
+
+    public void paintThis(TextView textView) {
+        if (!isPainted) {
+            isPainted = true;
+            textView.setTextColor(SharedPrefs.getInstance().getSelectedColor());
+        } else
+            textView.setTextColor(ContextCompat.getColor(textView.getContext(),
+                    R.color.lavaRed));
+    }
+
+    public void paintThis(ShimmerTextView shimmerTextViewtextView) {
+        if (!isPainted) {
+            isPainted = true;
+            shimmerTextViewtextView.setTextColor(SharedPrefs.getInstance().getSelectedColor());
+        } else
+            shimmerTextViewtextView.setTextColor(ContextCompat.getColor(shimmerTextViewtextView.getContext(),
+                    R.color.lavaRed));
+    }
+
+    public void paintThis(SearchView.SearchAutoComplete searchViewText) {
+        if (!isPainted) {
+            isPainted = true;
+            searchViewText.setTextColor(SharedPrefs.getInstance().getSelectedColor());
+        } else
+            searchViewText.setTextColor(ContextCompat.getColor(searchViewText.getContext(),
+                    R.color.lavaRed));
+    }
+
+    public void paintThis(SoundAdapter.ViewHolder holder) {
+        if (!isPainted) {
+            isPainted = true;
+            holder.itemView.setBackgroundColor(SharedPrefs.getInstance().getSelectedColor());
+        } else
+            holder.itemView.setBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(),
+                    R.color.lavaRed));
+    }
+
+    public void paintThis(FloatingActionButton fab) {
+        if (!isPainted) {
+            isPainted = true;
+            fab.setRippleColor(SharedPrefs.getInstance().getSelectedColor());
+        } else
+            fab.setRippleColor(ContextCompat.getColor(fab.getContext(),
+                    R.color.lavaRed));
+    }
+
+    public void initPaypal(Activity activity) {
+        String url =
+                "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=68KTBN3PE9U46";
+        Intent intent = new Intent(Intent.ACTION_VIEW).setData((Uri.parse(url)));
+        activity.startActivity(intent);
+    }
+
+    public void initYoutube(Activity activity) {
+        String url = "https://www.youtube.com/subscription_center?add_user=TheToNouSou96";
+        Intent intent = new Intent(Intent.ACTION_VIEW).setData((Uri.parse(url)));
+        activity.startActivity(intent);
+    }
+
+    public void initGithub(Activity activity) {
+        String url = "https://github.com/xtonousou/xSoundBoardHD";
+        Intent intent = new Intent(Intent.ACTION_VIEW).setData((Uri.parse(url)));
+        activity.startActivity(intent);
+    }
+
+    public boolean isGreenMode(Activity activity) {
+        boolean mode = false;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP &&
+                ((PowerManager) activity.getSystemService(Context.POWER_SERVICE))
+                        .isPowerSaveMode()) {
+            mode = true;
+        }
+        return mode;
+    }
+
+    public void restartActivity(Activity activity) {
+        activity.finish();
+        activity.startActivity(activity.getIntent());
+    }
+}
