eebdb4c8d9189a358db64454bcfa8cd631b1cd32
xtonousou <xtonousou@gmail.com>
2017-12-05 00:01:43 +0200

    Refactored and optimized sounds.

---
+++ scripts/cleanmp3.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+eyeD3 --remove-all-comments --remove-all-lyrics --remove-all-images --remove-v2 --remove-v1 --remove-all-objects --remove-all *
--- scripts/sourceme.sh
+++ scripts/sourceme.sh
@@ -4,3 +4,4 @@ alias mp4tomp3="/home/xtonousou/docs/projects/androidstudioprojects/SoundBoardX/
 alias optimp3="/home/xtonousou/docs/projects/androidstudioprojects/SoundBoardX/scripts/./optimp3.sh"
 alias optimpng="/home/xtonousou/docs/projects/androidstudioprojects/SoundBoardX/scripts/./optipng.sh"
 alias tosvg="/home/xtonousou/docs/projects/androidstudioprojects/SoundBoardX/scripts/./tosvg.sh"
+alias tosvg="/home/xtonousou/docs/projects/androidstudioprojects/SoundBoardX/scripts/./cleanmp3.sh"
