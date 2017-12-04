503d39f210bace5e5a18742e399c8066cdb772d5
Sotirios M. Roussis <xtonousou@gmail.com>
2017-12-05 01:18:59 +0200

    Updated project dir.

---
+++ scripts/removecontributor.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+git filter-branch -f --env-filter '
+OLD_EMAIL="kkelly@brightcove.com"
+CORRECT_NAME="xtonousou"
+CORRECT_EMAIL="xtonousou@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
