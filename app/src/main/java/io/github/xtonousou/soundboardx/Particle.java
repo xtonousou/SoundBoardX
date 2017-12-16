e9aca2cc876c7240dcdfdf7e01ca60852a42d139
xtonousou <xtonousou@gmail.com>
2017-12-16 21:02:46 +0200

    Minor corrections on particle emitters

--- app/src/main/java/io/github/xtonousou/soundboardx/Particle.java
+++ app/src/main/java/io/github/xtonousou/soundboardx/Particle.java
@@ -14,32 +14,32 @@ import java.util.ArrayList;
 
 class Particle {
 
-    private final View view;
+    private final View mView;
+    private Activity mActivity;
 
     Particle(View view) {
-        this.view = view;
+        this.mView = view;
+        this.mActivity = (Activity) view.getContext();
     }
 
     void burstRandomly(ArrayList<Integer> drawables, int particles) {
-        Activity act = (Activity) view.getContext();
         for (byte i = 0; i < drawables.size(); i++) {
-            new ParticleSystem(act, particles, drawables.get(i), 2500)
+            new ParticleSystem(mActivity, particles, drawables.get(i), 2500)
                     .setSpeedRange(0.1f, 0.35f)
                     .setFadeOut(750)
                     .setScaleRange(0.9f, 1.3f)
                     .setRotationSpeedRange(0, 200)
-                    .oneShot(view, particles, new DecelerateInterpolator());
+                    .oneShot(mView, particles, new DecelerateInterpolator());
         }
     }
 
     void burstScaleRandomly(ArrayList<Integer> drawables, int particles) {
-        Activity act = (Activity) view.getContext();
         for (byte i = 0; i < drawables.size(); i++) {
-            new ParticleSystem(act, particles, drawables.get(i), 2500)
+            new ParticleSystem(mActivity, particles, drawables.get(i), 2500)
                     .setSpeedRange(0.1f, 0.3f)
                     .setFadeOut(750)
                     .setScaleRange(1, 2.25f)
-                    .oneShot(view, particles, new DecelerateInterpolator());
+                    .oneShot(mView, particles, new DecelerateInterpolator());
         }
     }
 }
