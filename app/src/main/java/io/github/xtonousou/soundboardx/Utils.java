9fc1b1e601d02af4586c4774a4b2a1c0bed4fcd6
xtonousou <xtonousou@gmail.com>
2017-12-19 02:21:01 +0200

    Removed unused function inside utils

--- app/src/main/java/io/github/xtonousou/soundboardx/Utils.java
+++ app/src/main/java/io/github/xtonousou/soundboardx/Utils.java
@@ -136,14 +136,6 @@ abstract class Utils {
 		toolbar.setBackgroundColor(toolbar.getResources().getColor(R.color.colorPrimaryDarker));
     }
 
-	int getIntFromColor(int Red, int Green, int Blue){
-		Red = (Red << 16) & 0x00FF0000; //Shift red 16-bits and mask out other stuff
-		Green = (Green << 8) & 0x0000FF00; //Shift Green 8-bits and mask out other stuff
-		Blue = Blue & 0x000000FF; //Mask out anything not blue.
-
-		return 0xFF000000 | Red | Green | Blue; //0xFF000000 for 100% Alpha. Bitwise OR everything together.
-	}
-
 	static boolean isGreenMode(Activity activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
 			if (((PowerManager) activity.getSystemService(Context.POWER_SERVICE)).isPowerSaveMode())
